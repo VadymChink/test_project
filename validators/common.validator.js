d0873390e2f32d0f1f44dbd5964c9146a8d5b9a4
VadymChink <vadim-vsnnschuk@ro.ru>
2022-08-26 12:15:39 +0300

    validators

--- validators/common.validator.js
+++ validators/common.validator.js
@@ -3,12 +3,12 @@ const {constants} = require("../constants");
 
 module.exports = {
     email: joi.string().regex(constants.EMAIL_REGEX).trim().lowercase(),
-    categories: joi.array().items(joi.string()),
+    categories: joi.array().items(joi.string().valid('nodejs', 'angular', 'javascript', 'react')),
     japaneseKnowledge: joi.boolean(),
-    level: joi.string(),
+    level: joi.string().valid('junior', 'middle', 'senior'),
     japaneseRequired: joi.boolean(),
     description: joi.string(),
-    category: joi.string(),
+    category: joi.string().valid('nodejs', 'angular', 'javascript', 'react'),
     company: joi.string(),
 
 }
